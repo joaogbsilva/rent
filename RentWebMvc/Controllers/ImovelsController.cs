85b63b908c5a45c176ab707b467bc2c546cb121a
João Guilherme <joaogbsilva@gmail.com>
2021-05-12 14:59:44 -0300

    Recreate ImovelsController and View Imovels

---
+++ RentWebMvc/Controllers/ImovelsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentWebMvc.Models;
+
+namespace RentWebMvc.Controllers
+{
+    public class ImovelsController : Controller
+    {
+        private readonly RentWebMvcContext _context;
+
+        public ImovelsController(RentWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Imovels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Imovel.ToListAsync());
+        }
+
+        // GET: Imovels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imovel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imovel);
+        }
+
+        // GET: Imovels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Imovels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Logradouro,Numero,Complemento,Bairro,CEP,Cidade,Estado,Alugado")] Imovel imovel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(imovel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imovel);
+        }
+
+        // GET: Imovels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imovel.FindAsync(id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+            return View(imovel);
+        }
+
+        // POST: Imovels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logradouro,Numero,Complemento,Bairro,CEP,Cidade,Estado,Alugado")] Imovel imovel)
+        {
+            if (id != imovel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(imovel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImovelExists(imovel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imovel);
+        }
+
+        // GET: Imovels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imovel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imovel);
+        }
+
+        // POST: Imovels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var imovel = await _context.Imovel.FindAsync(id);
+            _context.Imovel.Remove(imovel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImovelExists(int id)
+        {
+            return _context.Imovel.Any(e => e.Id == id);
+        }
+    }
+}

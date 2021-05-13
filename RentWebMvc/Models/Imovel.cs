303581a2f6a002798b6d1da19c1f69dcbedcdc3f
João Guilherme <joaogbsilva@gmail.com>
2021-05-13 00:55:59 -0300

    Inputs validates

--- RentWebMvc/Models/Imovel.cs
+++ RentWebMvc/Models/Imovel.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RentWebMvc.Models
 {
 	public class Imovel
-	{
+	{		
 		public int Id { get; set; }
-		public string Logradouro { get; set; }
+		[Required(ErrorMessage = "O campo Rua é obrigatório")] public string Logradouro { get; set; }
 		public string Numero { get; set; }
 		public string Complemento { get; set; }
-		public string Bairro { get; set; }
-		public string CEP { get; set; }
-		public string Cidade { get; set; }
-		public string Estado { get; set; }
+		[Required(ErrorMessage = "O campo Bairro é obrigatório")] public string Bairro { get; set; }
+		[Required(ErrorMessage = "O campo CEP é obrigatório")]	public string CEP { get; set; }
+		[Required(ErrorMessage = "O campo Cidade é obrigatório")] public string Cidade { get; set; }
+		[Required(ErrorMessage = "O campo Estado é obrigatório")] public string Estado { get; set; }
 		public bool Alugado { get; set; }
 	}
 }
--- RentWebMvc/Startup.cs
+++ RentWebMvc/Startup.cs
@@ -61,7 +61,7 @@ namespace RentWebMvc
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+					template: "{controller=Imovels}/{action=Index}/{id?}");
 			});
 		}
 	}
